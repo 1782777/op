322183552bbdb216301a395aab59de7071bfbb6c
1782777 <771279043@qq.com>
2019-09-28 18:00:53 +0800

    fix abnormal_scan

--- abnormal_scan.py
+++ abnormal_scan.py
@@ -14,7 +14,18 @@ class scan():
         self.load =LoadNet()
         self.months = self.load.check_month()
         self.data_panel = pd.DataFrame()
-        
+        self.fig = plt.figure()
+        self.tmp =0.0
+        self.ax = self.fig.gca(projection='3d')
+        timer = self.fig.canvas.new_timer(interval=1000)
+        timer.add_callback(self.draw)
+        timer.start()
+        plt.show()
+
+ 
+
+    def draw(self):
+        self.data_panel = pd.DataFrame()
         for month in self.months:
             up ,down = self.load.get_op_codes(month)
             xqj_list =[]
@@ -23,7 +34,7 @@ class scan():
                 data = self.load.get_op_greek_alphabet(code)
                 xqj_list.append(float(data[13]))
                 iv_list.append(float(data[9]))
-            df=pd.DataFrame(iv_list,index=xqj_list,columns=[month+'_购'])
+            df=pd.DataFrame(iv_list,index=xqj_list,columns=[month])
             self.data_panel = pd.concat([self.data_panel,df],axis=1)
             xqj_list =[]
             iv_list=[]
@@ -31,33 +42,43 @@ class scan():
                 data = self.load.get_op_greek_alphabet(code)
                 xqj_list.append(float(data[13]))
                 iv_list.append(float(data[9]))
-            df=pd.DataFrame(iv_list,index=xqj_list,columns=[month+'_沽'])
+            df=pd.DataFrame(iv_list,index=xqj_list,columns=[month])
             self.data_panel = pd.concat([self.data_panel,df],axis=1)
-        #data_panel = pd.to_numeric(data_panel)
+        
         self.data_panel.index =pd.to_numeric(self.data_panel.index)
         self.data_panel.columns =[1,2,3,4,5,6,7,8]
         print (self.data_panel)
-        # self.data_panel.plot()
-        # plt.show()
 
-    def draw(self):
-        # x = np.array(self.data_panel.index)
-        # y =np.array(self.data_panel.columns)
-        x = np.arange(1,7,1)
-        print(x)
+        x = np.array(self.data_panel.index)
+        y =np.array(self.data_panel.columns)
+       
+        X,Y =np.meshgrid(y,x)
+        print(X.shape,Y.shape)
+        Z = np.array(self.data_panel)
+        self.tmp =self.tmp+0.2
+        Z[5,5] =self.tmp
+        # fig = plt.figure()
+        # ax = fig.gca(projection='3d')
+        surf = self.ax.plot_surface(X, Y, Z,rcount=100, ccount=100, cmap=cm.coolwarm, edgecolor='green')
+        self.ax.figure.canvas.draw()
         
-        y = np.arange(1,15,1)
-        print(y)
-        z = np.array(self.data_panel)
-        print(z)
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-        fig.colorbar(surf, shrink=0.5, aspect=5)
- 
+        
+
+    def testShow(self):
+        theta = 2 * np.pi * np.random.random(1000)
+        r = 6 * np.random.random(1000)
+        x = np.ravel(r * np.sin(theta))
+        y = np.ravel(r * np.cos(theta))
+        z = np.sin(np.sqrt(x ** 2 + y ** 2))
+        ax = plt.axes(projection='3d')
+        ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5)
         plt.show()
+        
 
 if __name__ == '__main__':
     s = scan()
-    s.draw()
+    #s.draw()
+
+    
+
+   
